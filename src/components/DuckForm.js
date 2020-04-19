8a527f76510df13fa4bf535020e289b9be5461c0
Al Rosenthal <alfred.k.rosentahl@gmail.com>
2020-04-19 13:34:06 -0700

    wired up submit button with API request

--- src/components/DuckForm.js
+++ src/components/DuckForm.js
@@ -7,25 +7,48 @@ import {
 	FormHelperText,
 	Input,
 } from '@material-ui/core';
+import { DateTimePicker } from '@material-ui/pickers';
+
+import './DuckForm.css';
 
 export default class DuckForm extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			feedingTime: '',
-			foodType: '',
-			foodAmount: '',
-			location: '',
-			numberOfDucks: '',
-			repeatFeeding: '',
+			form: {
+				feedingTime: '2020-04-19',
+				foodType: 'Bread',
+				foodAmount: '10',
+				location: 'The lake',
+				numberOfDucks: 10,
+				repeatFeeding: true,
+			},
 		};
-		console.log(new Date());
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
+
 	handleChange() {
 		console.log('An input changed...');
 	}
-	handleSubmit() {
-		console.log('Handle Submission');
+	async handleSubmit(e) {
+		e.preventDefault();
+		console.log('Form Submission');
+		const { form } = this.state;
+		console.log(form);
+		const headers = new Headers();
+		headers.append('Content-Tye', 'application/json');
+		const options = {
+			method: 'POST',
+			headers,
+			body: JSON.stringify(form),
+		};
+		const saveData = new Request('http://localhost:8080/feeding', options);
+		try {
+			const response = await fetch(saveData);
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
 	render() {
@@ -36,53 +59,49 @@ export default class DuckForm extends React.Component {
 			location,
 			numberOfDucks,
 			repeatFeeding,
-		} = this.state;
+		} = this.state.form;
 		return (
 			<div>
-				<form onSubmit={this.handleSubmit}>
+				<form className={'form'} onSubmit={this.handleSubmit}>
 					<FormControl>
-						<InputLabel htmlFor='datetime-local'>Feeding time</InputLabel>
-						<DateTimePicker
+						{/* <DateTimePicker
 							value={feedingTime}
 							disablePast
 							onChange={this.handleChange}
 							label='With Today Button'
 							showTodayButton
-						/>
+						/> */}
 					</FormControl>
 					<FormControl>
 						<InputLabel htmlFor='my-input'>Type of food</InputLabel>
 						<Input id='my-input' aria-describedby='my-helper-text' />
-						<FormHelperText id='my-helper-text'>
-							We'll never share your email.
-						</FormHelperText>
 					</FormControl>
 					<FormControl>
 						<InputLabel htmlFor='my-input'>Amount of food</InputLabel>
-						<Input id='my-input' aria-describedby='my-helper-text' />
-						<FormHelperText id='my-helper-text'>
-							We'll never share your email.
-						</FormHelperText>
+						<Input
+							id='my-input'
+							aria-describedby='my-helper-text'
+							type='number'
+						/>
 					</FormControl>
 					<FormControl>
 						<InputLabel htmlFor='my-input'>
 							Where did you feed the ducks
 						</InputLabel>
 						<Input id='my-input' aria-describedby='my-helper-text' />
-						<FormHelperText id='my-helper-text'>
-							We'll never share your email.
-						</FormHelperText>
 					</FormControl>
 					<FormControl>
 						<InputLabel htmlFor='my-input'>
 							How many ducks did you feed
 						</InputLabel>
-						<Input id='my-input' aria-describedby='my-helper-text' />
-						<FormHelperText id='my-helper-text'>
-							We'll never share your email.
-						</FormHelperText>
+						<Input
+							id='my-input'
+							aria-describedby='my-helper-text'
+							type='number'
+						/>
 					</FormControl>
 					<FormControl>
+						Do you do this everyday?
 						<Checkbox
 							color='primary'
 							value='checkedA'
@@ -90,7 +109,7 @@ export default class DuckForm extends React.Component {
 						/>
 					</FormControl>
 
-					<Button variant='contained' color='primary'>
+					<Button variant='contained' color='primary' type='submit'>
 						Feed the ducks
 					</Button>
 				</form>
