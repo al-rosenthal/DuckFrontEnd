299926856d61902a7b630d89fffac17476fe0210
Al Rosenthal <alfred.k.rosentahl@gmail.com>
2020-04-19 12:06:25 -0700

    added basic form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+	Button,
+	FormControl,
+	InputLabel,
+	FormHelperText,
+	Input,
+} from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				{/* <img src={logo} className='App-logo' alt='logo' /> */}
+				<p>Feed those ducks.</p>
+				<form>
+					<FormControl>
+						<InputLabel htmlFor='my-input'>Email address</InputLabel>
+						<Input id='my-input' aria-describedby='my-helper-text' />
+						<FormHelperText id='my-helper-text'>
+							We'll never share your email.
+						</FormHelperText>
+					</FormControl>
+
+					<Button>I've fed the ducks</Button>
+				</form>
+			</header>
+		</div>
+	);
 }
 
 export default App;
